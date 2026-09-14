a68d8049c1bb43b866fc0cebdce236e3285bb212
Harsh <hpprajapati1910@gmail.com>
2026-09-14 19:54:27 +0530

    Time: 932 ms (41.25%), Space: 12.4 MB (78.43%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        m = len(s)
+        n = len(p)
+
+        dp = [False] * (n + 1)
+        dp[0] = True
+
+        for j in range(1, n + 1):
+            if p[j - 1] == '*':
+                dp[j] = dp[j - 1]
+
+        for i in range(1, m + 1):
+            prev = dp[0]
+            dp[0] = False
+
+            for j in range(1, n + 1):
+                temp = dp[j]
+
+                if p[j - 1] == '*':
+                    dp[j] = dp[j] or dp[j - 1]
+                elif p[j - 1] == '?' or p[j - 1] == s[i - 1]:
+                    dp[j] = prev
+                else:
+                    dp[j] = False
+
+                prev = temp
+
+        return dp[n]
